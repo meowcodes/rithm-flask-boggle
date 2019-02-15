49326ad855afb0ba721ab9ef90990ee39072dd00
annika <annikaslund@gmail.com>
2019-02-15 14:43:48 -0800

    Add score

--- static/script.js
+++ static/script.js
@@ -9,6 +9,12 @@ $guessForm.on("submit", async function(evt) {
     // sending guess, getting back result
     let response = await $.post("/check-guess", {"guess" : $guess})
 
+    if (response.result === "ok"){
+        let curr_score = Number($('#score').text())
+        curr_score += $guess.length
+        $("#score").text(curr_score)
+    }
     // displaying result in page
     $("#result-div").text(response.result)
 })
+
